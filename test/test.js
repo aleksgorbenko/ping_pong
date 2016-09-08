af71a714696b99275333e821fc5d4f353bb4c7d3
Aleks Gorbenko <kucher12@gmail.com>
2016-09-08 21:13:30 +0100

    added test descriptions

---
+++ test/test.js
@@ -0,0 +1,29 @@
+var chai = require('chai');
+var $ = require('jquery');
+var fs = require('fs');
+
+describe('add a game button', function() {
+  it('should add the game to the "latest-games" div', function() {
+
+  });
+
+  it('should NOT add the game if player1 and player2 are the same', function() {
+
+  });
+
+  it('should NOT add the game if either of players is not choosen', function() {
+
+  });
+});
+
+describe('trash icon button', function() {
+  it('should remove the selected game', function() {
+
+  });
+});
+
+describe('game function', function() {
+  it('should return >= 2 and < 6 scores', function() {
+
+  });
+});
