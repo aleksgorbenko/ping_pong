389b44b4b59b3e9a5a2aa37d81ce4c6b2a55fc3d
Aleks Gorbenko <kucher12@gmail.com>
2016-09-20 23:54:33 +0100

    fixed typo and styles

--- gulpfile.js
+++ gulpfile.js
@@ -9,29 +9,31 @@ var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 var cssClean = require('gulp-clean-css');
 
-gulp.task('build-css', function () {
-  return gulp.src(assetsDev + '/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({}).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(autoprefixer({
-      browsers: ['last 3 versions'],
-      cascade: false
-    }))
-    .pipe(cssClean({ compatibility: 'ie8' }))
-    .pipe(gulp.dest(assetsProd));
+gulp.task('build-css', function() {
+    return gulp.src(assetsDev + '/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(cssClean({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest(assetsProd));
 });
 
-gulp.task('webserver', function () {
-  gulp.src('./')
-    .pipe(webserver({
-      livereload: true,
-      fallback: "index.html"
-    }));
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            fallback: "index.html"
+        }));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(assetsDev + '/*.scss', ['build-css']);
+gulp.task('watch', function() {
+    gulp.watch(assetsDev + '/*.scss', ['build-css']);
 });
 
 gulp.task('default', ['watch', 'build-css']);
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,7 @@
 $(function() {
     // Removes the game from the page
     $('.latest-games').on('click', '.glyphicon-trash', function removeGame() {
-        var input = confirm('Are you sure you want this game removed?"]');
+        var input = confirm('Are you sure you want this game removed?');
         if (input === true) {
             $(this).parent('.game').remove();
         } else {
